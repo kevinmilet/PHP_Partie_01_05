719992abc7096a0a5f6f6eeb8266e7c05976c49d
kevinmilet <kevin.milet@gmail.com>
2020-12-09 15:43:17 +0100

    partie 4 ex 6

---
+++ PHP_Partie_04/ex06.php
@@ -0,0 +1,8 @@
+<?php
+
+function func($lastname = 'Bonisseur de la Batte', $firstname = 'Hubert', $age = 35)
+{
+    echo 'Bonjour ' . $firstname . ' ' . $lastname . ', tu as ' . $age . ' ans.';
+}
+
+func();
