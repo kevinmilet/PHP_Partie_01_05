418d78e05674a63591471143e77a7feac3573cd9
kevinmilet <kevin.milet@gmail.com>
2020-12-09 16:40:41 +0100

    partie 5 ex 4 ok

---
+++ PHP_Partie_05/ex04.php
@@ -0,0 +1,23 @@
+<?php
+
+$months = array(
+    'janvier',
+    'février',
+    'mars',
+    'avril',
+    'mai',
+    'juin',
+    'juillet',
+    'aout',
+    'septembre',
+    'octobre',
+    'novembre',
+    'décembre'
+);
+
+echo $months[7];
+echo '<br>';
+
+array_splice($months, 7, 1, 'août');
+
+echo $months[7];
