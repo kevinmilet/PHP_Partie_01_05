422cface983eb3d8af233a2bc54f54ef7fd85a3f
kevinmilet <kevin.milet@gmail.com>
2020-12-09 11:45:23 +0100

    partie 2 ex 2 re-up

--- PHP_Partie_02/ex02.php
+++ PHP_Partie_02/ex02.php
@@ -5,4 +5,11 @@ if ($isEasy == true) {
     echo "C'est facile !!";
 } else {
     echo "C'est difficile !!";
-}
+    echo "<br>";
+}?>
+
+<?php if ($isEasy == true): ?>
+    C'est facile !!
+<?php else: ?>
+    C'est difficile !!
+<?php endif; ?>
