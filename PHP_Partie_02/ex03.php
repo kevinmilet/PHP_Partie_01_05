bb7816f674c13f00ab832d91b0c74d97b8be88a2
kevinmilet <kevin.milet@gmail.com>
2020-12-09 11:56:17 +0100

    partie 2 ex 3 ok

--- PHP_Partie_02/ex03.php
+++ PHP_Partie_02/ex03.php
@@ -1,2 +1,14 @@
 <?php
 
+$age = 15;
+$gender = 'homme';
+
+if ($age >= 18 && $gender == 'homme') {
+    echo "Vous êtes un homme et vous êtes majeur";
+} elseif ($age >= 18 && $gender == 'femme') {
+    echo "Vous êtes une femme et vous êtes majeure";
+} elseif ($age < 18 && $gender == 'homme') {
+    echo "Vous êtes un homme et vous êtes mineur";
+} elseif ($age < 18 && $gender == 'femme') {
+    echo "Vous êtes une femme et vous êtes mineure";
+}
