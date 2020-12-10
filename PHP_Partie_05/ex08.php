66be9af462d970d9083cf35a5c5cef4671ef61e7
kevinmilet <kevin.milet@gmail.com>
2020-12-10 09:25:47 +0100

    partie 5 ex 8 ok

---
+++ PHP_Partie_05/ex08.php
@@ -0,0 +1,21 @@
+<?php
+
+$months = [
+    'janvier',
+    'février',
+    'mars',
+    'avril',
+    'mai',
+    'juin',
+    'juillet',
+    'aout',
+    'septembre',
+    'octobre',
+    'novembre',
+    'décembre'
+];
+
+foreach ($months as $value) {
+    echo $value;
+    echo '<br>';
+};
