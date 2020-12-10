189dcef69c767b0bcaadd0ac8d8b869503b23440
kevinmilet <kevin.milet@gmail.com>
2020-12-10 09:22:07 +0100

    partie 5 ex 7 ok

---
+++ PHP_Partie_05/ex07.php
@@ -0,0 +1,13 @@
+<?php
+
+$departements = [
+    '02' => 'Aisne',
+    '59' => 'Nord',
+    '60' => 'Oise',
+    '62' => 'Pas-de-Calais',
+    '80' => 'Somme'
+];
+
+$departements[51] = 'Marne';
+
+print_r($departements);
